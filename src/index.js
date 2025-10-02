1527b4f9746c617b459d545dd9da1ed615532b4b
Michael Orford <morford@trihydro.com>
2025-10-02 09:33:02 -0600

    chore: update-react-to-new-major-version

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
 // Bind the React app to <div id="root">
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
